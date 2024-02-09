9c6d45cfa09a03eca81a4311aac84864d7dbba0d
hdmor <hdmor.h@gmail.com>
2024-02-09 12:42:57 +0330

    Use AuthenticatedLayout.vue in home page to display navbar(with some small changes in navbar)

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,6 +4,7 @@ import GroupList from "@/Components/app/GroupList.vue";
 import FollowerList from "@/Components/app/FollowingList.vue";
 import CreatePost from "@/Components/app/CreatePost.vue";
 import PostList from "@/Components/app/PostList.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 defineProps({});
 </script>
@@ -11,18 +12,20 @@ defineProps({});
 <template>
     <Head title="Home"/>
 
-    <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
-        <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-            <GroupList/>
+    <AuthenticatedLayout>
+        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+            <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+                <GroupList/>
+            </div>
+            <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+                <FollowerList/>
+            </div>
+            <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
+                <CreatePost/>
+                <PostList class="flex-1"/>
+            </div>
         </div>
-        <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
-            <FollowerList/>
-        </div>
-        <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
-            <CreatePost/>
-            <PostList class="flex-1"/>
-        </div>
-    </div>
+    </AuthenticatedLayout>
 </template>
 
 <style>
